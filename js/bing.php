389a270f22f6e41ead6872a37123b1d1a6858f2c
py l <1418172546@qq.com>
2022-01-12 00:47:45 +0800

    添加项目文件。

---
+++ js/bing.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+    <?php
+    $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+    $data = json_decode($str);
+    $imghost = 'https://cn.bing.com';
+    $imgpath = $data->{'images'}[0]->{'url'};
+    if ($imgpath) {
+        $imgurl = $imghost . $imgpath;
+        header('Location:' . $imgurl);
+        exit();
+    } else {
+        exit('error');
+    }
+    ?>
+</body>
+
+</html>
